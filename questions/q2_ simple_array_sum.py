d720b3dfa3c517f07bae7426dc3359a82a92460a
mahmudurmahid <m.mahid@icloud.com>
2025-05-19 16:06:04 +0100

    added alt solution for q2

--- questions/q2_ simple_array_sum.py
+++ questions/q2_ simple_array_sum.py
@@ -6,6 +6,18 @@ def simple_array_sum(ar):
 
     return total
 
+# alternative solution
+def alt_simple_array_sum(ar):
+    total = 0
+
+    for i in ar:
+        total += i
+    
+    return total
+
 arr = [1, 2, 3, 4, 10, 11]
 result = simple_array_sum(arr)
 print(result)
+
+alt_result = alt_simple_array_sum(arr)
+print(alt_result)
