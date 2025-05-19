812657f580f0f8966fa650b7b9457cd176f1a4a3
mahmudurmahid <m.mahid@icloud.com>
2025-05-19 16:41:00 +0100

    solved q7 - staircase

---
+++ questions/q7_staircase.py
@@ -0,0 +1,12 @@
+def staircase(n):
+    x = 0
+    figure = []
+
+    while x <= n:
+        spaces = " " * (n - x)
+        hashes = "#" * x
+        print(spaces, hashes)
+        x += 1
+
+result = staircase(6)
+print(result)
