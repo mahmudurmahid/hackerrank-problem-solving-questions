22cfd9613521adbcd4fc08fd6adecdaf176c4f89
mahmudurmahid <m.mahid@icloud.com>
2025-05-19 15:41:00 +0100

    solved q1 - solve me first

---
+++ questions/q1_solve_me_first.py
@@ -0,0 +1,9 @@
+def solve_me_first(a, b):
+    sum = a + b
+    return sum
+
+num1 = int(input("enter a number"))
+num2 = int(input("enter another number"))
+
+resolution = solve_me_first(num1, num2)
+print(resolution)
