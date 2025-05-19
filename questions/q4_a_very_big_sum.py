80ed07af0531b57c1dbc3ab39bee2e0cdb9ec1f4
mahmudurmahid <m.mahid@icloud.com>
2025-05-19 16:07:27 +0100

    added alt solution for q4

--- questions/q4_a_very_big_sum.py
+++ questions/q4_a_very_big_sum.py
@@ -6,6 +6,17 @@ def a_very_big_sum(ar):
     
     return total
 
+# alternative solution
+def alt_a_very_big_sum(ar):
+    total = 0
+
+    for i in ar:
+        total += i
+    
+    return total
+
 arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
 result = a_very_big_sum(arr)
 print(result)
+alt_result = alt_a_very_big_sum(arr)
+print(alt_result)
