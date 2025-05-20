b30dd92d0c49cb0ca92458949c6629e11ed65f73
mahmudurmahid <m.mahid@icloud.com>
2025-05-20 10:40:04 +0100

    solution for q12 - apples and oranges

---
+++ questions/q12_apple_and_orange.py
@@ -0,0 +1,31 @@
+def count_apples_and_oranges(s, t, a, b, apples, oranges):
+    apples_landed = 0
+    oranges_landed = 0
+
+    for apple in apples:
+        if s <= apple + a <= t:
+            apples_landed += 1
+        else: # s > apple + a > t
+            apples_landed = apples_landed
+    
+    for orange in oranges:
+        if s <= orange + b <= t:
+            oranges_landed += 1
+        else: # s > orange + b > t
+            oranges_landed = oranges_landed
+    
+    fruit_landed = [apples_landed, oranges_landed]
+    return fruit_landed
+
+
+s = 7
+t = 10
+a = 4
+b = 12
+m = 3
+n = 3
+apples_arr = [2, 3, -4]
+oranges_arr = [3, -2, -4]
+
+result = count_apples_and_oranges(s, t, a, b, apples_arr, oranges_arr)
+print(result)
