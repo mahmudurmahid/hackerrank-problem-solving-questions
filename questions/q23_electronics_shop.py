fb7f5b9f96e12798a45d155eba1912cc68e29a72
mahmudurmahid <m.mahid@icloud.com>
2025-05-27 12:09:57 +0100

    solution for q23

---
+++ questions/q23_electronics_shop.py
@@ -0,0 +1,23 @@
+def get_money_spent(keyboards, drives, b):
+    total_prices = []
+    affordable_options = []
+
+    for n in range(len(keyboards)):
+        for m in range(len(drives)):
+            total_prices.append(keyboards[n] + drives[m])
+
+    for i in range(len(total_prices)):
+        if total_prices[i] <= b:
+            affordable_options.append(total_prices[i])
+        else: # total_prices[i] >= b
+            affordable_options.append(-1)
+        
+    most_expensive = max(affordable_options)
+    
+    return most_expensive
+
+b = 60
+keyboards = [40, 50, 60]
+drives = [5, 8, 12]
+result = get_money_spent(keyboards, drives, b)
+print(result)
