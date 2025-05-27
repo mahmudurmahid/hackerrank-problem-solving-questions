16e260cc202ad3d7fe264816e6caa8422df69964
mahmudurmahid <m.mahid@icloud.com>
2025-05-27 17:57:21 +0100

    unsolved q26

---
+++ questions/q25_forming_s_magic_square.py
@@ -0,0 +1,33 @@
+def forming_magic_square(s):
+    magic_squares = [
+        [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+        [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+        [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+        [[2, 9, 4], [7, 5, 3], [6, 1, 8]],
+        [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+        [[4, 3, 8], [9, 5, 1], [2, 7, 6]],
+        [[6, 7, 2], [1, 5, 9], [8, 3, 4]],
+        [[2, 7, 6], [9, 5, 1], [4, 3, 8]],
+    ]
+
+    min_cost = float('inf')
+    
+    for magic in magic_squares:
+        cost = 0
+        for i in range(3):
+            for j in range(3):
+                cost += abs(s[i][j] - magic[i][j])
+        min_cost = min(min_cost, cost)
+    
+    return min_cost
+
+    
+
+
+s = [
+    [5, 3, 4],
+    [1, 5, 8],
+    [6, 7, 2]
+    ]
+result = forming_magic_square(s)
+print(result)
---
+++ questions/q26_picking_numbers.py
@@ -0,0 +1,14 @@
+def picking_numbers(a):
+    pass
+
+
+
+
+
+
+
+
+
+a = [1, 1, 2, 2, 4, 4, 5, 5, 5]
+result = picking_numbers(a)
+print(result)
