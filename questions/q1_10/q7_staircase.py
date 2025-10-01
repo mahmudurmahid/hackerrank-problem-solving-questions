ed94458b0a884551f1d688e37244557219c98f2e
mahmudurmahid <m.mahid@icloud.com>
2025-10-01 15:58:20 +0100

    alternative solution for q7

--- questions/q1_10/q7_staircase.py
+++ questions/q1_10/q7_staircase.py
@@ -7,5 +7,17 @@ def staircase(n):
         print(spaces, hashes)
         x += 1
 
+# alternative solution
+def staircase(n):
+    y = 1
+
+    while y != (n + 1):
+        spaces = " " * (n - y)
+        hashes = "#" * y
+        print(spaces + hashes)
+        y += 1
+
 result = staircase(6)
 print(result)
+alt_result = staircase(6)
+print(alt_result)
