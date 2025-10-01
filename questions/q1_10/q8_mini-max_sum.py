2b66a164feedee73b8e4ca4f21bbf334c1ecff46
mahmudurmahid <m.mahid@icloud.com>
2025-10-01 17:05:31 +0100

    alternative solution for q8

--- questions/q1_10/q8_mini-max_sum.py
+++ questions/q1_10/q8_mini-max_sum.py
@@ -7,6 +7,24 @@ def mini_max_sum(arr):
 
     return max_total, min_total
 
+def alt_mini_max_sum(arr):
+    ascending_arr = sorted(arr)
+    descending_arr = sorted(arr, reverse=True)
+    
+    max_sum = 0
+    min_sum = 0
+
+    for number in range(len(ascending_arr) - 1):
+        min_sum += ascending_arr[number]
+    
+    for number in range(len(descending_arr) - 1):
+        max_sum += descending_arr[number]
+    
+    return max_sum, min_sum
+
+
 arr = [1, 3, 5, 7, 9]
 result = mini_max_sum(arr)
 print(result)
+alt_result = alt_mini_max_sum(arr)
+print(alt_result)
