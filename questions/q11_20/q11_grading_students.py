1a9bbbb781ac15659205bc0b6aedc1ea9869d688
mahmudurmahid <m.mahid@icloud.com>
2025-10-09 18:12:16 +0100

    alt solution for q11

--- questions/q11_20/q11_grading_students.py
+++ questions/q11_20/q11_grading_students.py
@@ -16,6 +16,24 @@ def grading_students(grades):
     
     return final_grades
 
+# alternative solution
+def alt_grading_students(grades):
+    final_grades = []
+
+    for grade in grades:
+        if grade < 38:
+            final_grades.append(grade)
+        else:
+            rounded_grade = 5 * math.ceil(grade / 5)
+            if rounded_grade - grade < 3:
+                final_grades.append(rounded_grade)
+            else:
+                final_grades.append(grade)
+
+    return final_grades
+
 grades_arr = [4, 73, 67, 38, 33]
 result = grading_students(grades_arr)
 print(result)
+alt_result = alt_grading_students(grades_arr)
+print(alt_result)
