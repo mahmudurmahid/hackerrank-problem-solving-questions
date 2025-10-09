1e4f488d4654f3ed0df840687e60e1dc9142cd34
mahmudurmahid <m.mahid@icloud.com>
2025-10-09 16:56:47 +0100

    another alt solution for q10

--- questions/q1_10/q10_time_conversion.py
+++ questions/q1_10/q10_time_conversion.py
@@ -29,9 +29,26 @@ def alt_time_conversion(s):
     
     return f"{hh}:{mm}:{ss}"
 
+# second alternative solution
+def alt2_time_conversion(s):
+    hour = int(s[:2])
+    minute = int(s[3:5])
+    second = int(s[6:8])
+
+    meridian = s[-2:]
+    
+    if meridian == "PM" and hour != 12 :
+        hour = hour + 12
+    elif meridian == "AM" and hour == 12:
+        hour = 00
+
+    military_time = f"{hour:02}:{minute:02}:{second:02}"
+    return military_time
 
 s = "12:05:00PM"
-result = time_conversion(s)
-print(result)
-alt_result = alt_time_conversion(s)
-print(alt_result)
+# result = time_conversion(s)
+# print(result)
+# alt_result = alt_time_conversion(s)
+# print(alt_result)
+alt2_result = alt2_time_conversion(s)
+print(alt2_result)
