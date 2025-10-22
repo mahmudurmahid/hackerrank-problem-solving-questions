3a974154f81f503426338120e46f2afa447bcdde
mahmudurmahid <m.mahid@icloud.com>
2025-10-22 16:43:56 +0100

    alt solution added

--- questions/q11_20/q15_subarray_division.py
+++ questions/q11_20/q15_subarray_division.py
@@ -9,8 +9,15 @@ def birthday(s, d, m):
     
     return num_combinations
 
+# alternative solution
 def alt_birthday(s, d, m):
+    total = 0
 
+    for number in range(len(s) + m - 1):
+        if sum(s[number:number + m]) == d:
+            total += 1
+    
+    return total
 
 s = [2, 2, 1, 3, 2]
 d = 4
