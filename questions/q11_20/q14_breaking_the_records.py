620a32048dab5f22f1ef121f7ee2edf5030174ba
mahmudurmahid <m.mahid@icloud.com>
2025-10-22 16:05:33 +0100

    alt solution admin

--- questions/q11_20/q14_breaking_the_records.py
+++ questions/q11_20/q14_breaking_the_records.py
@@ -19,6 +19,9 @@ def breaking_records(scores):
     score_record = (max_broken, min_broken)
     return score_record
 
+def alt_breaking_records(scores):
+    
+
 
 scores_arr = [12, 24, 10, 24]
 scores_arr2 = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]
@@ -26,3 +29,5 @@ result = breaking_records(scores_arr)
 print(result)
 result2 = breaking_records(scores_arr2)
 print(result2)
+alt_result = alt_breaking_records(scores_arr)
+print(alt_result)
