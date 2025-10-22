2f1758f150bf196b2bb42b719852485bc364c336
mahmudurmahid <m.mahid@icloud.com>
2025-10-22 16:16:57 +0100

    removed second alt solution as too similar to first

--- questions/q11_20/q14_breaking_the_records.py
+++ questions/q11_20/q14_breaking_the_records.py
@@ -19,29 +19,10 @@ def breaking_records(scores):
     score_record = (max_broken, min_broken)
     return score_record
 
-def alt_breaking_records(scores):
-    highest_score = scores[0]
-    lowest_score = scores[0]
-
-    highest_record_broken = 0
-    lowest_record_broken = 0
-
-    for score in scores:
-        if score > highest_score:
-            highest_record_broken += 1
-            highest_score = score
-        if score < lowest_score:
-            lowest_record_broken += 1
-            lowest_score = score
-    
-    return highest_record_broken, lowest_record_broken
-
-
 scores_arr = [12, 24, 10, 24]
 scores_arr2 = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42]
+
 result = breaking_records(scores_arr)
 print(result)
 result2 = breaking_records(scores_arr2)
 print(result2)
-alt_result = alt_breaking_records(scores_arr)
-print(alt_result)
