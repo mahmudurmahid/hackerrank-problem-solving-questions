1f360bf7215bb7b12e50c2dcc426e80cf4ea7fec
mahmudurmahid <m.mahid@icloud.com>
2025-10-23 16:29:09 +0100

    deleted alt solution as too similar to first

--- questions/q11_20/q16_divisible_sum_pairs.py
+++ questions/q11_20/q16_divisible_sum_pairs.py
@@ -10,21 +10,7 @@ def divisible_sum_pairs(ar, k):
     
     return num_pairs
 
-def alt_divisible_sum_pairs(ar, k):
-    total = 0
-
-    for i in range(len(ar)):
-        # print(arr[i])
-        for j in range(i+1, len(ar)):
-            # print(arr[j])
-            if (ar[i] + ar[j]) % k == 0:
-                total += 1
-    
-    return total
-
 arr = [1, 3, 2, 6, 1, 2]
 k = 3
 result = divisible_sum_pairs(arr, k)
 print(result)
-alt_result = alt_divisible_sum_pairs(arr, k)
-print(alt_result)
