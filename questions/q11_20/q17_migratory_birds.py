add389c947a9417ff14302f57d4496b22cf1a786
mahmudurmahid <m.mahid@icloud.com>
2025-10-24 14:17:08 +0100

    alt solution added

--- questions/q11_20/q17_migratory_birds.py
+++ questions/q11_20/q17_migratory_birds.py
@@ -21,7 +21,23 @@ def migratory_birds(ar):
     return max_bird_id
 
 def alt_migratory_birds(ar):
-    pass
+    birds_dict = {}
+
+    for bird in ar:
+        if bird in birds_dict:
+            birds_dict[bird] += 1
+        else:
+            birds_dict[bird] = 1
+    
+    most_seen_bird = 0
+    most_seen_bird_sightings = 0
+
+    for key, value in birds_dict.items():
+        if value > most_seen_bird:
+            most_seen_bird = key
+            most_seen_bird_sightings = value
+    
+    return f"The most seen bird was {most_seen_bird} which was seen {most_seen_bird_sightings} times"
 
 arr = [1, 1, 2, 2, 3, 2]
 result = migratory_birds(arr)
