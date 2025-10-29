5703d6d4aae158f32d648d26093d702e2eee06f8
mahmudurmahid <m.mahid@icloud.com>
2025-10-29 16:08:58 +0000

    alt solution for q22

--- questions/q21_30/q22_counting_valleys.py
+++ questions/q21_30/q22_counting_valleys.py
@@ -15,11 +15,33 @@ def counting_valleys(steps, path):
 
 # alternative solution
 def alt_counting_valleys(steps, path):
-    pass
+    sea_level = 0
+    valley = 0
 
-steps = 8
-path = ["U", "D", "D", "D", "U", "D", "U", "U"]
-result = counting_valleys(steps, path)
-print(result)
-alt_result = alt_counting_valleys(steps, path)
-print(alt_result)
+    for i in path:
+        if i == "U":
+            sea_level += 1
+            # print(f"step {sea_level}")
+        else:
+            sea_level -= 1
+            # print(f"step {sea_level}")
+        
+        if sea_level == 0 and i == "U":
+            valley += 1
+    
+    return valley
+
+steps1 = 8
+path1 = ["U", "D", "D", "D", "U", "D", "U", "U"]
+steps2 = 12
+path2 = ["D", "D", "U", "U", "D", "D", "U", "D", "U", "U", "U", "D"]
+
+result1 = counting_valleys(steps1, path1)
+result2 = counting_valleys(steps2, path2)
+print(result1)
+print(result2)
+
+alt_result1 = alt_counting_valleys(steps1, path1)
+alt_result2 = alt_counting_valleys(steps2, path2)
+print(alt_result1)
+print(alt_result2)
