a6f1bb9b2b75351904af0d7fcf6dc26ec3991bd2
mahmudurmahid <m.mahid@icloud.com>
2025-11-04 15:02:59 +0000

    solution for q26

--- questions/q21_30/q26_between_two_sets.py
+++ questions/q21_30/q26_between_two_sets.py
@@ -1,6 +1,15 @@
-def between_two_sets(a, b):
-    pass
+import math
 
+def between_two_sets(a, b):
+    l = math.lcm(*a) # find LCM of first array
+    g = math.gcd(*b) # find GCD of second array
+    count = 0
+
+    for i in range(l, g+1, l):
+        if (g % i) == 0:
+            count += 1
+    
+    return count
 
 a = [2, 6]
 b = [24, 36]
