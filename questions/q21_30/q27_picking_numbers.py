99d4fd1aa863402a7a1bd9be521affd98fd67f14
mahmudurmahid <m.mahid@icloud.com>
2025-11-04 15:28:18 +0000

    q27 unsolved - testing

--- questions/q21_30/q27_picking_numbers.py
+++ questions/q21_30/q27_picking_numbers.py
@@ -1,8 +1,22 @@
 def picking_numbers(a):
-    pass
+    # count = 0
+    current_array = []
+
+    # for i in a:
+    #     current_array.append(i)
+    #     for j in a:
+    #         if abs(i - j) <= 1:
+    #             current_array.append(j)
+    #             print(current_array)
+    
+    for i, j in enumerate(a):
+        current_array.append(a[i])
+        if abs(a[i] - j) <= 1:
+            current_array.append(j)
+            print(current_array)
 
 a1 = [4, 6, 5, 3, 3, 1]
 a2 = [1, 2, 2, 3, 1, 2]
 
 result1 = picking_numbers(a1)
-result2 = picking_numbers(a2)
+# result2 = picking_numbers(a2)
